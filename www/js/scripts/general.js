8ffd0e19db8438283d80c920dc59fa068296227b
Samiro <crisanto112@gmail.com>
2013-12-03 05:59:36 -0500

    Se agregaron las ventanas de compartir y acerca de

--- www/js/scripts/general.js
+++ www/js/scripts/general.js
@@ -7,3 +7,21 @@ $.loading = function(accion, texto){
 		$("#loading").hide()
 	}
 }
+
+$( "#window_compartir, #window_acerca" ).tap(function(event){
+	$(this).css("display", "none")
+})
+
+
+
+
+
+
+$( ".tap-acerca" ).tap( function( event ){
+	$("#window_acerca").css("display", "block")
+})
+
+$( ".tap-compartir" ).tap( function( event ){
+	$("#window_compartir").css("display", "block")
+})
+

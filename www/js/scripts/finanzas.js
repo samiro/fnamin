7536bea851f71e25945ddc25830cbfd202523bab
Samiro <crisanto112@gmail.com>
2014-02-04 19:17:58 -0500

    Servicios desde Set de datos

--- www/js/scripts/finanzas.js
+++ www/js/scripts/finanzas.js
@@ -24,7 +24,7 @@ var Insertar = {
 			$("#notaEgr").val("")
 		}
 	},
-	
+
 	ver_historia: function(accion){
 		if(accion == 'show'){
 			$("#mes").hide()
@@ -43,12 +43,12 @@ var Insertar = {
         db.transaction(
         	function(tx){
         		tx.executeSql(
-	        		"SELECT * FROM ingresos WHERE id = ?", 
-	        		[id], 
+	        		"SELECT * FROM ingresos WHERE id = ?",
+	        		[id],
 	        		function(tx, results){
 	        			if(results.rows.length>0){
 	        				navigator.notification.alert(
-	        					"Desea borrar el ingreso de "+dar_formato(results.rows.item(0).valor_ing) + " de " + results.rows.item(0).tipo_ing + "?", 
+	        					"Desea borrar el ingreso de "+dar_formato(results.rows.item(0).valor_ing) + " de " + results.rows.item(0).tipo_ing + "?",
 	        					function(){
 	        						tx.executeSql(
 	        							"DELETE FROM ingresos WHERE id = ?",
@@ -59,17 +59,17 @@ var Insertar = {
 	        								console.log("eliminar_ingreso: error eliminando ")
 	        							})
 	        					},
-	        					"Borrar ingreso", 
+	        					"Borrar ingreso",
 	        					"Borrar");
 	        			}
-	        		}, 
+	        		},
 	        		function(error){
 			            console.log("eliminar_ingreso: consultando id")
 			        });
-	        }, 
+	        },
 	        function(){
 	        	console.log("eliminar_ingreso: Error consultando 1 ")
-	        }, 
+	        },
 	        function(){
 	        	console.log("eliminar_ingreso: Exito consultando 2 ")
 	        });
@@ -80,12 +80,12 @@ var Insertar = {
         db.transaction(
         	function(tx){
         		tx.executeSql(
-	        		"SELECT * FROM egresos WHERE id = ?", 
-	        		[id], 
+	        		"SELECT * FROM egresos WHERE id = ?",
+	        		[id],
 	        		function(tx, results){
 	        			if(results.rows.length>0){
 	        				navigator.notification.alert(
-	        					"Desea borrar el egreso de $"+dar_formato(results.rows.item(0).valor_eg) + " de " + results.rows.item(0).tipo_eg + "?", 
+	        					"Desea borrar el egreso de $"+dar_formato(results.rows.item(0).valor_eg) + " de " + results.rows.item(0).tipo_eg + "?",
 	        					function(){
 	        						tx.executeSql(
 	        							"DELETE FROM egresos WHERE id = ?",
@@ -96,17 +96,17 @@ var Insertar = {
 	        								console.log("eliminar_egreso: error eliminando ")
 	        							})
 	        					},
-	        					"Borrar egreso", 
+	        					"Borrar egreso",
 	        					"Borrar");
 	        			}
-	        		}, 
+	        		},
 	        		function(error){
 			            console.log("eliminar_egreso: consultando id")
 			        });
-	        }, 
+	        },
 	        function(){
 	        	console.log("eliminar_egreso: Error consultando 1 ")
-	        }, 
+	        },
 	        function(){
 	        	console.log("eliminar_egreso: Exito consultando 2 ")
 	        });
@@ -118,13 +118,13 @@ var Insertar = {
 		var d=new Date();
         var dat=d.getDate();
         var mon=d.getMonth();
-        var year=d.getFullYear();        
+        var year=d.getFullYear();
        // var hoy = year+"-"+setDateZero(mon)+"-"+setDateZero(dat);
         $( "#mes" ).val(setDateZero(mon+1));
         configurar_db()
         RealizarLaConsulta();
 	});
-	
+
 	var ing_vacios = "";
 	var egr_vacios = "";
 	function configurar_db(){
@@ -145,17 +145,17 @@ var Insertar = {
         var db = window.openDatabase("bd_finanzas", "1.0", "Mis finanzas", 200000);
         db.transaction(execute , error, exito);
     }
-	
+
 	function setDateZero(date){
 		return date < 10 ? '0' + date : date;
     }
-	
+
 	function RealizarLaConsulta(tx) {
        RealizarConsultaIngre();
        RealizarConsultaEgre();
        RealizarSumaIngresos();
     }
-	
+
 	function GuardarNuevoIngreso() {
         var db = window.openDatabase("bd_finanzas", "1.0", "Mis finanzas", 200000);
         db.transaction(AgregarIngreso, errorOperacion, function(){
@@ -181,7 +181,7 @@ var Insertar = {
 			}
 		});
     }
-	
+
 	function isNumber(value) {
 	    if ((undefined === value) || (null === value)) {
 	        return false;
@@ -210,27 +210,27 @@ var Insertar = {
         var hoy = anio+"-"+setDateZero(mes)+"-"+setDateZero(dia);
         var tipo = $("#ingreso [name='tipoIng']").val();
         var nota = $("#ingreso input[name='notaIng']").val();
-				
+
 		ing_vacios = "";
-		
+
 		if(valor != ""){
 			if(valor.length <= 9){
 				if (isInteger(valor)){
 					var montoValidar =valor.toString();
-					var primerNumero =montoValidar.substring(0,1);  
+					var primerNumero =montoValidar.substring(0,1);
 					//console.log(primerNumero);
 					if(primerNumero != "0"){
 						if(!isNaN(valor) && valor > 0 && valor.indexOf(".") == -1 && valor.indexOf(",")== -1){
 							tx.executeSql('INSERT INTO ingresos (fecha_ing, valor_ing, tipo_ing, nota_ing) VALUES ("'+hoy+'", "'+valor+'", "'+tipo+'","'+nota+'")');
 						}else{
 							ing_vacios= "NN";
-						}	
+						}
 					}else{
 							ing_vacios= "CERO";
-						}	
+						}
 				}else{
 					ing_vacios = "NO_INT"
-				}				
+				}
 			}else{
 				ing_vacios= "NG";
 			}
@@ -238,25 +238,25 @@ var Insertar = {
 			ing_vacios = "S";
 		}
     }
-	
+
 	function errorOperacion(err) {
 		console.log("Finanzas:errorOperacion: ")
 		console.log(err)
         //navigator.notification.alert("Ocurrió un fallo, por favor vuelve a intentarlo.", function(){}, "Error", "Aceptar");
     }
-	
+
 	function RealizarConsultaIngre() {
         var db = window.openDatabase("bd_finanzas", "1.0", "Mis finanzas", 200000);
         db.transaction(consultaIngresos, errorOperacion, exito);
     }
 
     function consultaIngresos(tx) {
-       		
+
 		 var mesSeleccionado = $("#mes").val();
 		 var fechas = fechaConsultas(mesSeleccionado);
-		 var fechasVec= new Array(); 
+		 var fechasVec= new Array();
 		 var fechasVec = fechas.split('/');
-		 
+
         tx.executeSql("SELECT * FROM ingresos WHERE fecha_ing >='" +fechasVec[0]+ "'AND fecha_ing <='" +fechasVec[1]+"'", [], ResultadosIngresos, function(error){
             console.log("consultaIngresos error: " + error)
         });
@@ -265,22 +265,22 @@ var Insertar = {
     function ResultadosIngresos(tx, results) {
 		var len = results.rows.length;
         $("#entradas-ingresos").html("");
-		
+
 ;		if(len > 0){
 			for (var i=0; i<len; i++){
 				var nota = " Sin comentario ";
 				if(results.rows.item(i).nota_ing != ""){
 					nota = results.rows.item(i).nota_ing;
-				}	
-				var formatValor = dar_formato(results.rows.item(i).valor_ing);				
+				}
+				var formatValor = dar_formato(results.rows.item(i).valor_ing);
 				$("#entradas-ingresos").append('<div class="entrada-finanzas" onclick="Insertar.eliminar_ingreso('+results.rows.item(i).id+')"><label class="fecha">'+results.rows.item(i).fecha_ing+"  "+results.rows.item(i).tipo_ing+'</label><label class="valor">$'+formatValor+'</label><label class="fecha">'+nota+'</label></div>')
-			}		
+			}
 		}else{
 			$("#entradas-ingresos").append('<div class="entrada-finanzas"><label class="fecha">No hay registro de ingresos en este mes</label><label class="valor">$0</label><label class="fecha">  </label></div>')
 		}
        RealizarSumaIngresos();
     }
-	
+
 	 function RealizarSumaIngresos() {
         var db = window.openDatabase("bd_finanzas", "1.0", "Mis finanzas", 200000);
         db.transaction(SumarIngresos, errorOperacion, exito);
@@ -289,9 +289,9 @@ var Insertar = {
     function SumarIngresos(tx) {
          var mesSeleccionado = $("#mes").val();
 		 var fechas = fechaConsultas(mesSeleccionado);
-		 var fechasVec= new Array(); 
+		 var fechasVec= new Array();
 		 var fechasVec = fechas.split('/');
-		 
+
         tx.executeSql("SELECT SUM(valor_ing) as totalIng FROM ingresos WHERE fecha_ing >='" +fechasVec[0]+ "'AND fecha_ing <='" +fechasVec[1]+"'",[], ResSumaIngresos, errorOperacion);
     }
 
@@ -299,11 +299,11 @@ var Insertar = {
         window.ingresos = results.rows.item(0).totalIng;
         SumarEgresos(tx);
     }
-	
+
 	function SumarEgresos(tx) {
 		var mesSeleccionado = $("#mes").val();
 		var fechas = fechaConsultas(mesSeleccionado);
-		var fechasVec= new Array(); 
+		var fechasVec= new Array();
 		var fechasVec = fechas.split('/');
 		tx.executeSql("SELECT SUM(valor_eg) as totalEg FROM egresos WHERE fecha_eg >='" +fechasVec[0]+ "'AND fecha_eg <='" +fechasVec[1]+"'",[], SaldoTotal, errorOperacion);
     }
@@ -313,7 +313,7 @@ var Insertar = {
 		var dif = window.ingresos - egresos
 		var	rtado = dar_formato(dif);
         var diferencia = "$" +rtado;
-		
+
 		if(dif < 0){
 			$("#tit_ahorro").text("Ha gastado más de lo que ha ganado");
 			$("#sActual").html(diferencia);
@@ -324,21 +324,27 @@ var Insertar = {
 			$("#sActual").html(diferencia);
 			$("#sActual").css("color","#1A3C8F");
 			$("#pie_ahorro").text("Mejore su plan de gastos según su presupuesto.");
-		}else{
+		} else if(dif => window.ingresos * 0.1){
+            /*Si la diferencia es igual o superior al 10% de los ingresos*/
+            $("#tit_ahorro").text("Ha ahorrado en este periodo");
+            $("#sActual").html(diferencia);
+            $("#sActual").css("color","#C1D82F");
+            $("#pie_ahorro").text("Puede acercarse a una oficina FNA para invertir su ahorro.");
+        } else{
 			$("#tit_ahorro").text("Ha ahorrado en este periodo");
 			$("#sActual").html(diferencia);
 			$("#sActual").css("color","#C1D82F");
-			$("#pie_ahorro").text("Puede acercarse a una oficina fna para invertir su ahorro.");
+			$("#pie_ahorro").text(".");
 		}
     }
-	
+
 	function exito() {
       	console.log("Exito")
     }
-	
+
 	function listoAgregarEgreso() {
         var db = window.openDatabase("bd_finanzas", "1.0", "Mis finanzas", 200000);
-        db.transaction(AgregarEgreso, errorOperacion, function(){		
+        db.transaction(AgregarEgreso, errorOperacion, function(){
 			if(egr_vacios=="S"){
                 navigator.notification.alert("Debe diligenciar el campo valor, éste debe ser numérico y sin puntos.", function(){}, "Campo valor inválido", "Aceptar");
 			}
@@ -361,7 +367,7 @@ var Insertar = {
 			}
 		});
 	}
-   
+
    function AgregarEgreso(tx) {
         var today=new Date();
         var dia=today.getDate();
@@ -377,7 +383,7 @@ var Insertar = {
 			if(valor.length <= 9){
 				if (isInteger(valor)){
 					var montoValidar = valor.toString();
-					var primerNumero = montoValidar.substring(0,1);  
+					var primerNumero = montoValidar.substring(0,1);
 					if(primerNumero != "0"){
 						if(!isNaN(valor) && valor > 0 && valor.indexOf(".")== -1 && valor.indexOf(",")== -1){
 							tx.executeSql('INSERT INTO egresos (fecha_eg, valor_eg, tipo_eg, nota_eg) VALUES ("'+hoy+'", "'+valor+'", "'+tipo+'","'+nota+'")');
@@ -406,7 +412,7 @@ var Insertar = {
     function consultaEgresos(tx) {
         var mesSeleccionado = $("#mes").val();
 		var fechas = fechaConsultas(mesSeleccionado);
-		var fechasVec= new Array(); 
+		var fechasVec= new Array();
 		var fechasVec = fechas.split('/');
         tx.executeSql("SELECT * FROM egresos WHERE fecha_eg >='" +fechasVec[0]+ "'AND fecha_eg <='" +fechasVec[1]+"'", [], ResultadosEgresos, function(error){
             console.log("consultaEgresos error: " + error)
@@ -421,7 +427,7 @@ var Insertar = {
 				var nota = " Sin comentario ";
 				if(results.rows.item(i).nota_eg != ""){
 					nota = results.rows.item(i).nota_eg;
-				}	
+				}
 				var formatValor = dar_formato(results.rows.item(i).valor_eg);
 				$("#entradas-egresos").append('<div class="entrada-finanzas" onclick="Insertar.eliminar_egreso('+results.rows.item(i).id+')"><label class="fecha">'+results.rows.item(i).fecha_eg+" - "+results.rows.item(i).tipo_eg+'</label><label class="valor">$'+formatValor+'</label><label class="fecha">'+nota+'</label></div>')
 			}
@@ -430,9 +436,9 @@ var Insertar = {
 		}
         RealizarSumaIngresos();
     }
-	
+
 	function fechaConsultas(mesSeleccionado) {
-       
+
 		var d=new Date();
         var dat=d.getDate();
         var mon=d.getMonth();
@@ -441,7 +447,7 @@ var Insertar = {
 		var fd ="";
 		var fh ="";
 		//var Hoy = year +"-"+ setDateZero(monH) +"-"+ setDateZero(dat);
-		
+
 		if(mesSeleccionado == "1" || mesSeleccionado == "3" || mesSeleccionado == "5" || mesSeleccionado == "7" || mesSeleccionado == "8" || mesSeleccionado == "10" || mesSeleccionado == "12"){
 			var diaFin = "31";
 		}else if(mesSeleccionado == "2"){
@@ -449,7 +455,7 @@ var Insertar = {
 		}else{
 			var diaFin = "30";
 		}
-		
+
 		if(mesSeleccionado <= monH){
 			fd = year +"-"+ mesSeleccionado +"-"+ "01";
 			fh = year +"-"+ mesSeleccionado +"-"+ diaFin;
@@ -460,31 +466,31 @@ var Insertar = {
 		return fd + "/" + fh;
     }
 
-	
+
 	////////////////////////////
-function dar_formato(num){  
-  
+function dar_formato(num){
+
 	var cadena = "";
-	var aux; 
-	var cont = 0,m,k;  
-	 
-	num=num.toString();  	  	
-	for(m=num.length-1; m>=0; m--){ 	  
-		cadena = num.charAt(m) + cadena; 	  
+	var aux;
+	var cont = 0,m,k;
+
+	num=num.toString();
+	for(m=num.length-1; m>=0; m--){
+		cadena = num.charAt(m) + cadena;
 		cont++;
 		if(cont == 3 && num.length > 3)
 		{
-			cadena = "," + cadena; 
+			cadena = "," + cadena;
 		}
 		else if(cont == 6 && num.length >= 7)
 		{
-			cadena = "'" + cadena; 
+			cadena = "'" + cadena;
 		}
 		else if(cont == 9 && num.length > 10)
 		{
-			cadena = "," + cadena; 
+			cadena = "," + cadena;
 		}
-	}  
-	return cadena;  
-}  
+	}
+	return cadena;
+}
 
---
+++ www/js/scripts/portafolio.js
@@ -0,0 +1,87 @@
+var Portafolio = {
+    URL_DATOS: "http://servicedatosabiertoscolombia.cloudapp.net/v1/Fondo_Nacional_Ahorro/portafolioservfna?$format=json",
+    cargar_todos: function(){
+        $(".lista-portafolio").html("")
+        $.loading('show', 'Cargando servicios');
+        $.ajax({
+            url: Portafolio.URL_DATOS,
+            type: 'GET',
+            dataType: 'jsonp',
+            crossDomain: true,
+            success: function( data ) {
+                $.loading( 'hide' )
+                var servicios = data.d
+                for (var i = 0; i < servicios.length; i++) {
+                    var servicio = "<li><div><div class='btn-text'><a href=\"javascript: Portafolio.ir_a('" +servicios[i].RowKey+ "')\">"
+                        servicio+= '<h2>'+servicios[i].servicios+'</h2>'
+                        servicio+= '<p>'+servicios[i].mensaje+'</p></a></div><span class="icon">&nbsp;</span></div></li>'
+
+                    $(".lista-portafolio").append(servicio)
+                }
+                Portafolio.guardar_datos( servicios )
+            },
+            error: function (x, y, z) {
+                console.log("cargar_todos: Error cargando Portafolio Todos")
+            }
+        });
+    },
+    cargar_detalle: function( key ){
+        var key = window.localStorage.getItem("key_servicio");
+        if(key != undefined && key!=""){
+            var db = window.openDatabase("bd_servicios", "1.0", "Servicios", 200000);
+            db.transaction(function(tx){
+                tx.executeSql("SELECT * FROM servicios WHERE key = '" + key + "'", [],
+                    function(tx, results){
+                        if(results.rows.length > 0){
+                            var servicio = results.rows.item(0)
+                            $("#servicio-nombre").html(servicio.nombre_servicio)
+                            $("#servicio-imagen").html(servicio.logo)
+                            $("#servicio-titulo").html(servicio.titulo)
+                            $("#servicio-detalle").html(servicio.descripcion.replace(/•/g, "<br />•"))
+                            $("#servicio-beneficios").html(servicio.beneficios)
+                        }
+                    },
+                    function(error){
+                        console.log("cargar_detalle: error consultando")
+                        console.log(error)
+                    });
+            },
+            function(error){
+                console.log("cargar_detalle: error abriendo db")
+                console.log(error)
+            },
+            function(){
+                console.log("cargar_detalle: exito")
+            })
+        }
+    },
+    ir_a: function(key){
+        window.localStorage.setItem("key_servicio", key);
+        window.location.href = "__portafolio_detalle.html"
+    },
+    guardar_datos: function( datos ){
+        var db = window.openDatabase("bd_servicios", "1.0", "Servicios", 200000);
+        db.transaction(function(tx){
+            /*Ejecutar*/
+            tx.executeSql('CREATE TABLE IF NOT EXISTS servicios (key, nombre_servicio, mensaje, titulo, descripcion, beneficios, logo)');
+            for (var i = 0; i < datos.length; i++) {
+                var key = datos[i].RowKey
+                var nombre_servicio = datos[i].servicios
+                var mensaje = datos[i].mensaje
+                var titulo = datos[i].titulo
+                var descripcion = datos[i].descripciondelservicio
+                var beneficios = datos[i].beneficios
+                var logo = datos[i].logodelservicio
+                tx.executeSql('INSERT INTO servicios (key, nombre_servicio, mensaje, titulo, descripcion, beneficios, logo) VALUES ("'+key+'", "'+nombre_servicio+'", "'+mensaje+'", "'+titulo+'", "'+descripcion+'", "'+beneficios+'", "'+logo+'")');
+            }
+        },
+        function(error){
+            /*Error*/
+            console.log("Ha ocurrido un error al crear la tabla")
+            console.log(error)
+        },
+        function(){
+            console.log("guadar_datos portafolio bien")
+        });
+    }
+}
